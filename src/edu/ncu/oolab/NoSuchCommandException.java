27232e7e29ee42952036f9d57eb7f780acb05bea
Wei Cheng Pan <legnaleurc@gmail.com>
2012-05-06 10:04:31 +0800

    Add exception classes.

---
+++ src/edu/ncu/oolab/NoSuchCommandException.java
@@ -0,0 +1,10 @@
+package edu.ncu.oolab;
+
+public class NoSuchCommandException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5056860349113114794L;
+
+}
---
+++ src/edu/ncu/oolab/NoSuchIDException.java
@@ -0,0 +1,10 @@
+package edu.ncu.oolab;
+
+public class NoSuchIDException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2075986188269970218L;
+
+}
