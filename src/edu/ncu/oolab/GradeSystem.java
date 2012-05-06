49d87f85c34dce1f38731130ac60a7d325b6a3cf
Wei Cheng Pan <legnaleurc@gmail.com>
2012-05-06 13:40:17 +0800

    Implement grade system.

--- src/edu/ncu/oolab/GradeSystem.java
+++ src/edu/ncu/oolab/GradeSystem.java
@@ -1,6 +1,13 @@
 package edu.ncu.oolab;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GradeSystem {
 	
@@ -8,6 +15,42 @@ public class GradeSystem {
 		
 		private String name_, id_;
 		private Integer lab1_, lab2_, lab3_, midTerm_, final_, grade_;
+		
+		public Grade(String line) {
+			byte[] bytes = null;
+			try {
+				bytes = line.getBytes("UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			InputStream in = new ByteArrayInputStream(bytes);
+			Scanner sin = new Scanner(in, "UTF-8");
+			this.id_ = sin.next();
+			this.name_ = sin.next();
+			this.lab1_ =  sin.nextInt();
+			this.lab2_ = sin.nextInt();
+			this.lab3_ = sin.nextInt();
+			this.midTerm_ = sin.nextInt();
+			this.final_ = sin.nextInt();
+			sin.close();
+			try {
+				in.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			this.calculateGrade();
+		}
+		
+		private void calculateGrade() {
+			Double a = ( this.lab1_ * GradeSystem.this.weights_[0] );
+			Double b = ( this.lab2_ * GradeSystem.this.weights_[1] );
+			Double c = ( this.lab3_ * GradeSystem.this.weights_[2] );
+			Double d = ( this.midTerm_ * GradeSystem.this.weights_[3] );
+			Double e = ( this.final_ * GradeSystem.this.weights_[4] );
+			this.grade_ = (int) Math.floor( a + b + c + d + e );
+		}
 		
 	}
 	
@@ -24,6 +67,26 @@ public class GradeSystem {
 		this.weights_[3] = 0.3;
 		this.weights_[4] = 0.4;
 		this.cUser_ = null;
+		
+		FileInputStream f = null;
+		try {
+			f = new FileInputStream("input.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Scanner fin = new Scanner(f, "UTF-8");
+		while (fin.hasNextLine()) {
+			String line = fin.nextLine();
+			this.grades_.add(new Grade(line));
+		}
+		fin.close();
+		try {
+			f.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public Boolean selectByID(String id) {
@@ -42,6 +105,9 @@ public class GradeSystem {
 	
 	public void setWeights(Double[] weights) {
 		this.weights_ = weights;
+		for (Grade g : this.grades_) {
+			g.calculateGrade();
+		}
 	}
 	
 	public String getUserName() {
